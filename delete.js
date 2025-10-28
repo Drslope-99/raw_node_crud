b7bb4cc2291ed0138837905751abdcc2a299893d
Drslope-99 <enamsifon@gmail.com>
2025-10-28 18:33:41 +0100

    fixed the delete bug

--- delete.js
+++ delete.js
@@ -1,5 +1,4 @@
 export function deleteAddress(addresses, id) {
-  const parsedId = parseInt(id, 10);
-  const filteredAddress = addresses.filter((address) => address.id != parsedId);
+  const filteredAddress = addresses.filter((address) => address.id != id);
   return filteredAddress;
 }
